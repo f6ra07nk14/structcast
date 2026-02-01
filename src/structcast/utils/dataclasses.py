c297c79ba1dca6bf237d2bfb6ad286fea761e781
frank.kang <frank.kang@coretronic.com>
2026-02-02 01:17:25 +0800

    refactor: simplify dataclass utility by removing overloads and using dataclass_transform

--- src/structcast/utils/dataclasses.py
+++ src/structcast/utils/dataclasses.py
@@ -2,20 +2,13 @@
 
 import dataclasses
 import sys
-from typing import Any, Callable, Literal, Optional, TypeVar, Union, overload
+from typing import Any, Callable, Union
 
-T = TypeVar("T")
-
-
-@overload
-def dataclass(cls: type[T], **kwargs: Any) -> type[T]: ...
+from typing_extensions import dataclass_transform
 
 
-@overload
-def dataclass(cls: Literal[None] = None, **kwargs: Any) -> Callable[..., type[T]]: ...
-
-
-def dataclass(cls: Optional[type[T]] = None, **kwargs: Any) -> Union[Callable[..., type[T]], type[T]]:
+@dataclass_transform(field_specifiers=(dataclasses.field,))
+def dataclass(cls: Any = None, **kwargs: Any) -> Union[Callable[..., Any], Any]:
     """A wrapper around dataclasses.dataclass that adds kw_only and slots parameters for Python 3.10+."""
     other_kw = {"kw_only": True, "slots": True} if sys.version_info >= (3, 10) else {}
     return dataclasses.dataclass(cls, **{**other_kw, **kwargs})
