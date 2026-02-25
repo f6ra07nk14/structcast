6ca3d97e994247c92746d7170610dc94494b0785
frank.kang <frank.kang@coretronic.com>
2026-02-25 15:35:29 +0800

    feat: add base classes for structcast configurations

---
+++ src/structcast/core/base.py
@@ -0,0 +1,26 @@
+"""Base classes for structcast configurations."""
+
+from typing import Any, cast
+
+from pydantic import BaseModel, ConfigDict
+
+
+class Serializable(BaseModel):
+    """Base configuration."""
+
+    model_config = ConfigDict(frozen=True, validate_default=True, extra="forbid", serialize_by_alias=True)
+
+
+class WithExtra(Serializable):
+    """Base class for configurations with extra fields allowed."""
+
+    model_config = ConfigDict(extra="allow")
+
+    @property
+    def model_extra(self) -> dict[str, Any]:
+        """Get extra fields set during validation.
+
+        Returns:
+            A dictionary of extra fields, or `None` if `config.extra` is not set to `"allow"`.
+        """
+        return cast(dict[str, Any], self.__pydantic_extra__)
